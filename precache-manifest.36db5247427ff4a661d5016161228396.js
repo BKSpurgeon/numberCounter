d5fde5397e1e68b558e6e54a5019839933e60489
BK <benkoshy@hotmail.com>
2020-04-09 22:57:41 +1000

    Updates

---
+++ precache-manifest.36db5247427ff4a661d5016161228396.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819ec7b316700122c5f56fd16ec7144d",
+    "url": "/numberCounter/custom.css"
+  },
+  {
+    "revision": "ccf94ba236031291c1a0d9b21eac0fdb",
+    "url": "/numberCounter/favicon.ico"
+  },
+  {
+    "revision": "e4fe68503ab5d004deb31e43636a0a7c",
+    "url": "/numberCounter/giphy.png"
+  },
+  {
+    "revision": "b80af468738f0a3f345b540a939bfdd4",
+    "url": "/numberCounter/index.html"
+  },
+  {
+    "revision": "791f3109a9d53a227adfb76bb2393491",
+    "url": "/numberCounter/logo.png"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/numberCounter/logo.svg"
+  },
+  {
+    "revision": "894687c123552e1a90f5291937251d7b",
+    "url": "/numberCounter/manifest.json"
+  },
+  {
+    "revision": "e83fd87c7f0a19367756",
+    "url": "/numberCounter/static/css/main.1e0edaa0.chunk.css"
+  },
+  {
+    "revision": "e83fd87c7f0a19367756",
+    "url": "/numberCounter/static/js/main.e83fd87c.chunk.js"
+  },
+  {
+    "revision": "4288a6a41340618eeb0c",
+    "url": "/numberCounter/static/js/runtime~main.4288a6a4.js"
+  },
+  {
+    "revision": "7aa09ec89c957a98c55b",
+    "url": "/numberCounter/static/js/vendors~main.7aa09ec8.chunk.js"
+  }
+]);
